eafaff7bfa814a5bd92bf8f4fe9cab91fff97966
CoderFaveX <faveejiofor2009@gmail.com>
2024-06-12 07:57:19 +0100

    changing the uri

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ admin.initializeApp({
 const bucket = admin.storage().bucket();
 
 const uri =
-  "mongodb+srv://faveejiofor2009:ybffqUz8267uEFY6@portfolio.0ixnpy5.mongodb.net/?retryWrites=true&w=majority&appName=Portfolio";
+  "mongodb+srv://mufisiruakinsanya00:nCGY1Oc3MpjQEbgO@testdatabase.legb9e2.mongodb.net/?retryWrites=true&w=majority&appName=Testdatabase";
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
