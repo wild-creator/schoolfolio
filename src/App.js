06d9121385f409aa37e7b6de90236dc737667f75
CoderFaveX <faveejiofor2009@gmail.com>
2024-06-12 01:07:30 +0100

    Updating App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
 
     const fetchContacts = async () => {
         try {
-            const response = await axios.get('http://localhost:5000/api/contacts');
+            const response = await axios.get(`${window.location.origin}/api/user`);
             setContacts(response.data);
         } catch (error) {
             console.error('Error fetching contacts:', error);
